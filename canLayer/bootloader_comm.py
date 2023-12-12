c5bcc1cccd2e9df51fa21733af20642914bc8d03
octavio.guendulain <octavioraziel.gdn@gmail.com>
2023-12-12 13:31:58 -0600

    remove debug comments

--- canLayer/bootloader_comm.py
+++ canLayer/bootloader_comm.py
@@ -18,14 +18,11 @@ READOUT_UNPROTECT_COMMAND = 0x92
 
 def start_bootloader():
     print("Start bootloader: ")    
-    start_can_message = (CAN_ACK,0,0x00)
-    print(f"datos:{start_can_message} ")  #debug message it will be commented
+    start_can_message = (CAN_ACK,0,0x00)    
     can_bus=config_can_bus()
     send_can_message(can_bus, start_can_message)
     device_answer=get_chip_answer(CAN_ACK,can_bus)
-    can_bus.shutdown()
-    print(f"Device answer:{device_answer} ")  #debug message it will be commented
-    
+    can_bus.shutdown()    
     if device_answer == CAN_NO_ANSWER:
         print("sBoot No Answer\n")        
         return False
@@ -43,8 +40,7 @@ def config_can_bus():
     bus=can.Bus(interface='socketcan',
                 channel='can0',
                 receive_own_messages=False,
-                bitrate=500000)
-    print("Can bus configured.") #debug message it will be commented
+                bitrate=500000)    
     return bus
 
 def send_can_message(can_bus, can_message):
@@ -53,12 +49,9 @@ def send_can_message(can_bus, can_message):
                           dlc=can_message[1],
                           is_extended_id=True                         
                           )
-    can_bus.send(message, timeout=0.2)
-    #can_bus.shutdown()
-    print("Can message sent.") #debug message it will be commented
+    can_bus.send(message, timeout=0.2)    
 
-def get_chip_answer(can_ID, can_bus):   
-    print("Waiting for answer...") #debug message it will be commented 
+def get_chip_answer(can_ID, can_bus):       
     try:        
      receivedmessage = can_bus.recv(timeout=10.0)     
     except can.canError as e:
@@ -68,21 +61,16 @@ def get_chip_answer(can_ID, can_bus):
         try:
             print(f"Response received:{receivedmessage.arbitration_id},{receivedmessage.data}")
             if receivedmessage.arbitration_id == can_ID:            
-                if int.from_bytes(receivedmessage.data) == CAN_ACK:
-                    print("-ACK received.") #debug message it will be commented
+                if int.from_bytes(receivedmessage.data) == CAN_ACK:                    
                     return CAN_ACK
-                elif int.from_bytes(receivedmessage.data) == CAN_NACK:
-                    print("-NACK received.") #debug message it will be commented
+                elif int.from_bytes(receivedmessage.data) == CAN_NACK:                    
                     return CAN_NACK
-                else:
-                    print("-No valid data received.") #debug message it will be commented
+                else:                    
                     return CAN_NO_ANSWER         
-            else:
-                print("No valid  ID received.") #debug message it will be commented
+            else:                
                 return CAN_NO_ANSWER
         except AttributeError as tye:
             print(f"error of type error:{tye}") 
-            return CAN_NO_ANSWER            
-        
+            return CAN_NO_ANSWER
     finally:
         print("end of get_chip_answer")
--- st-can-flash-main.py
+++ st-can-flash-main.py
@@ -2,18 +2,17 @@ import argparse
 from canLayer.bootloader_comm import *
 def main():
     help_string = "Commands available.\ng: get the version and the allowed comands supported by the bootloader. \ni: get chip ID. \nr: read memory where x is 0, 2, 4, 8, 16, 32, 64 or 128. \ns: starts user application code.\nw: write memory with data from filename. \n e: erase memory."
-    parser = argparse.ArgumentParser(description="ST flash application. Enjoy it!")
+    parser = argparse.ArgumentParser(description="ST flash application. ")
     parser.add_argument("option", choices=["g", "i", "r", "s", "w", "e"], help = help_string)     
-    parser.add_argument("-x", type=int, help="number for the option r.")
-    parser.add_argument("-file", help="file to be written in memory.")
-    print("ST-Flash though CAN v0.3:\nRun as sudo st-can-flash. \n")      
+    parser.add_argument("-x", type=int, help="Number for the option r.")
+    parser.add_argument("-file", help="File to be written in memory.")
+    print("ST-Flash though CAN:\nRun as sudo st-can-flash. \n")      
     args = parser.parse_args()
     start_bootloader()
     print("Bootloader started.")
     print("Option selected:", args.option)
     if args.option == "g":
-        print("g Get the version and allowed commands.")
-         
+        print("g Get the version and allowed commands.")         
     elif args.option == "i":
         print("i Get chip ID.")
 
